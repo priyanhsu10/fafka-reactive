60460351d48f34d6fb7650cdb6b41350288f661d
priyanshu10 <priyanshuparate@gmail.com>
2025-03-05 21:56:40 +0530

    group consumer

--- 02-kafka-101/01-topic.sh
+++ 02-kafka-101/01-topic.sh
@@ -5,7 +5,15 @@ kafka-topics.sh --bootstrap-server locahost:9092 --topic order-event --create
 kafka-topics.sh --bootstrap-server localhost:9092 --list
 
 # describe more details for topic
-kafka-topic.sh --bootstrap-server localhost:9092 --topic order-event --describe
+kafka-topics.sh --bootstrap-server localhost:9092 --topic order-event --describe
 
 # delete the topic 
-kafka-topic.sh --boostrap-server localhost:9092 --topic order-event --delete
+kafka-topics.sh --boostrap-server localhost:9092 --topic order-event --delete
+
+
+kafka-topics.sh --bootstrap-server localhost:9092 --topic hello-world --delete
+
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic hello-world --property print.offset=true --group ps
+
+
+kafka-topics.sh --bootstrap-server localhost:9092 --topic hello-world --create --partitions 2
--- 02-kafka-101/03-producer.sh
+++ 02-kafka-101/03-producer.sh
@@ -1,3 +1,5 @@
 to produce the messages
 
-kafka-console-producer.sh --bootstrap-server localhost:9092 --topic order-event
+kafka-console-producer.sh --bootstrap-server localhost:9092 --topic hello-wd   --property key.separator=: --property parse.key=true
+
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic hello-wd   --property print.offset=true --property print.key=true  --group ps
