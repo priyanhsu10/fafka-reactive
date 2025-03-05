a8b1ad37f0cc463e02238948576e2250ad29d201
priyanshu10 <priyanshuparate@gmail.com>
2025-03-05 22:11:39 +0530

    describe consumer group

--- 02-kafka-101/05-consumer-group.sh
+++ 02-kafka-101/05-consumer-group.sh
@@ -1,5 +1,8 @@
 
 kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic hello-world --property print.offset=true --group ps
 
-    
+
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --list
+
+
+kafka-consumer-groups.sh --bootstrap-server localhost:9092 --group ps --describe
