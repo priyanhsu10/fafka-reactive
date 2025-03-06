56f3457b2bcd5bc33c0ecfb56d0af0c7304ec6c0
priyanshu10 <priyanshuparate@gmail.com>
2025-03-06 09:32:06 +0530

    reset offset

--- 02-kafka-101/05-consumer-group.sh
+++ 02-kafka-101/05-consumer-group.sh
@@ -1,5 +1,5 @@
 
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic hello-world --property print.offset=true --group ps
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic hello-wd --property print.offset=true --group ps
 
 
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --list
